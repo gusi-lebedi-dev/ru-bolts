a13b4cb6fc3c3fca89705fbc42687276547963e4
Nikita Rusakov <nikita.rusakov.z@gmail.com>
2024-08-06 16:28:53 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const elements = [
+    document.querySelector('.products-title'),
+    document.querySelector('.products-block'),
+    document.querySelector('.about-us-title'),
+    document.querySelector('.about-us-block-left'),
+    document.querySelector('.about-us-block-right'),
+    document.querySelector('.advantages-title'),
+    document.querySelector('.to-buy-title'),
+    document.querySelector('.to-buy-content-item-left'),
+    document.querySelector('.to-buy-content-item-right'),
+    document.querySelector('.to-buy-image'),
+    document.querySelector('.contact-title'),
+    document.querySelector('.contacts-content-item'),
+]
+
+document.querySelectorAll('.product-image').forEach(element => elements.push(element))
+document.querySelectorAll('.advantages-item-image').forEach(element => elements.push(element))
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+};
+
+const callback = (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+        }
+    });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+elements.forEach((element) => observer.observe(element));
+
+const openModal = () => {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const modal = document.querySelector('.modal');
+    if (modal.classList.contains('show')) {
+        modal.classList.remove('show');
+        burgerMenu.classList.remove('active')
+    } else {
+        modal.classList.add('show');
+        burgerMenu.classList.add('active')
+    }
+}
+
