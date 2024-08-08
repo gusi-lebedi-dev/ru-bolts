dfa1387ade74f5dacb23ec2214cf0d5c641d526d
Nikita Rusakov <nikita.rusakov.z@gmail.com>
2024-08-08 14:24:12 +0500

    change style

--- js/index.js
+++ js/index.js
@@ -1,54 +1,17 @@
-const elements = [
-    document.querySelector('.products-title'),
-    document.querySelector('.products-block'),
-    document.querySelector('.about-us'),
-    document.querySelector('.advantages-title'),
-    document.querySelector('.to-buy-title'),
-    document.querySelector('.to-buy-content-item-left'),
-    document.querySelector('.to-buy-content-item-right'),
-    document.querySelector('.to-buy-image'),
-    document.querySelector('.contact-title'),
-    document.querySelector('.contacts-content-item'),
-    document.querySelector('.appointment'),
-    document.querySelector('.section-use'),
-    document.querySelector('.section-characteristics'),
-    document.querySelector('.section-temperature-conditions'),
-    document.querySelector('.section-product-specification'),
-    document.querySelector('.section-lift-specification-content'),
-    document.querySelector('.section-helpful'),
-
-].filter(element => element !== null && element !== undefined);
-
-document.querySelectorAll('.product-image').forEach(element => {
-    if (element !== null && element !== undefined) {
-        elements.push(element);
-    }
-});
-
-document.querySelectorAll('.advantages-item-image').forEach(element => {
-    if (element !== null && element !== undefined) {
-        elements.push(element);
-    }
-});
-
-const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.1,
-};
-
-const callback = (entries) => {
+const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('active');
             observer.unobserve(entry.target);
         }
     });
-};
+}, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+});
 
-const observer = new IntersectionObserver(callback, options);
-
-elements.forEach((element) => observer.observe(element));
+document.querySelectorAll('[data-animate]').forEach((element) => observer.observe(element));
 
 const openModal = () => {
     const burgerMenu = document.querySelector('.burger-menu');
@@ -61,6 +24,3 @@ const openModal = () => {
         burgerMenu.classList.add('active')
     }
 }
-
-
-
